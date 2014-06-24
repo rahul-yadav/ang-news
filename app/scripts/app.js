665783a2f807390e6fdf5b6432292433bc968118
Rohit Yadav <rohit.gsxr@gmail.com>
2014-06-24 10:14:03 +0530

    <rahul>: adding files

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+/* global app:true */
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+var app = angular.module('angNewsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase'
+  ]);
+app.constant('FIREBASE_URL', 'https://brilliant-fire-4290.firebaseio.com/');
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/posts.html',
+      controller: 'PostsCtrl'
+    })
+    .when('/posts/:postId', {
+      templateUrl: 'views/showpost.html',
+      controller: 'PostViewCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
---
+++ app/scripts/controllers/posts.js
@@ -0,0 +1,17 @@
+'use strict';
+ 
+app.controller('PostsCtrl', function ($scope, $location, Post) {
+  $scope.posts = Post.all;
+
+  $scope.post = {url: 'http://', title: ''}; 
+  $scope.submitPost = function () {
+    Post.create($scope.post).then(function (ref) {
+       $location.path('/posts/' + ref.name());
+    });
+  };
+
+  $scope.deletePost = function (postId) {
+    Post.delete(postId);
+  };
+
+}); 
